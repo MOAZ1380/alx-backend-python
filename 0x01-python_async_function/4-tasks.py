ccd4f13e260a3c3feac06e63d74a1803a9ac695f
Your Name <you@example.com>
2024-10-14 16:30:00 +0000

    moaz

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -9,9 +9,9 @@ time_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    '''return a list of awaited response from previous function'''
-    lis = []
-    for i in range(n):
-        ma = await wait_random(max_delay)
-        lis.append(ma)
-    return sorted(lis)
+    '''return a list of awaited response from previous function
+    '''
+    res = await asyncio.gather(
+        *tuple(map(lambda _: time_wait_random(max_delay), range(n)))
+    )
+    return sorted(res)
