bebaf9b3af00f9e61bfab11a32fa08a695a65bf1
Your Name <you@example.com>
2024-10-14 14:31:59 +0000

    moaz

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''Asyncronos Python'''
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> int:
+    '''an asynchronous coroutine'''
+    wait = random.uniform(0, max_delay)
+    await asyncio.sleep(wait)
+    return wait
