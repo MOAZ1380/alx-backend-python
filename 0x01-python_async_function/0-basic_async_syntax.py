b7dc99ade5cdb71bd3e8d3fc7f4c8efb6dfa7106
Your Name <you@example.com>
2024-10-14 15:11:33 +0000

    g

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay: int = 10) -> int:
+async def wait_random(max_delay: int = 10) -> float:
     '''an asynchronous coroutine'''
     wait = random.random() * max_delay
     await asyncio.sleep(wait)
